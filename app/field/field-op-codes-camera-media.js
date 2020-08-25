c94eec80a325a31fe227f2bfb8d106160c05d3eb
Dan Garfield <dangarfielduk@hotmail.com>
2020-08-25 11:56:16 +0100

    add missing entity check for SCRLA

--- app/field/field-op-codes-camera-media.js
+++ app/field/field-op-codes-camera-media.js
@@ -58,12 +58,6 @@ const SCRCC = async (op) => {
 const SCRLA = async (op) => { // CHAR -> adds model.userData.entityId = entity script array i
     console.log('SCRLA', op)
     const speed = op.b == 0 ? op.s : getBankData(op.b, op.s)
-
-    const entity = window.currentField.models.filter(m => m.userData.entityId === op.e)[0]
-    // entityId is the array position according to the flevel.script.entities[]
-
-    let relativeToCamera = calculateViewClippingPointFromVector3(entity.scene.position)
-    console.log('SCRLA smooth?', op, getCurrentCameraPosition(), relativeToCamera)
     let tweenType = TweenType.Instant
     if (op.t === 1) {
         tweenType = TweenType.Instant
@@ -72,39 +66,58 @@ const SCRLA = async (op) => { // CHAR -> adds model.userData.entityId = entity s
     } else if (op.t === 3) {
         tweenType = TweenType.Smooth
     }
-    await tweenCameraPosition(getCurrentCameraPosition(), relativeToCamera, tweenType, speed, entity)
 
-    // Follow entity until it stops
-    let oldPosition = { x: entity.scene.position.x, y: entity.scene.position.y, z: entity.scene.position.z }
-    await sleep(1000 / 60) // Should probably follow the tick...
-    // console.log('entity.scene.position 2', oldPosition, entity.scene.position,
-    //     oldPosition.x !== entity.scene.position.x,
-    //     oldPosition.y !== entity.scene.position.y,
-    //     oldPosition.z !== entity.scene.position.z
-    // )
-    while (oldPosition.x !== entity.scene.position.x || oldPosition.y !== entity.scene.position.y || oldPosition.z !== entity.scene.position.z) {
-        oldPosition = { x: entity.scene.position.x, y: entity.scene.position.y, z: entity.scene.position.z }
+    const entities = window.currentField.models.filter(m => m.userData.entityId === op.e)
+    if (entities.length > 0) {
+        const entity = entities[0]
+        // entityId is the array position according to the flevel.script.entities[]
+
+        let relativeToCamera = calculateViewClippingPointFromVector3(entity.scene.position)
+        console.log('SCRLA smooth?', op, getCurrentCameraPosition(), relativeToCamera)
+
+        await tweenCameraPosition(getCurrentCameraPosition(), relativeToCamera, tweenType, speed, entity)
+
+        // Follow entity until it stops
+        let oldPosition = { x: entity.scene.position.x, y: entity.scene.position.y, z: entity.scene.position.z }
         await sleep(1000 / 60) // Should probably follow the tick...
-        // console.log('entity.scene.position LOOP', oldPosition, entity.scene.position,
+        // console.log('entity.scene.position 2', oldPosition, entity.scene.position,
         //     oldPosition.x !== entity.scene.position.x,
         //     oldPosition.y !== entity.scene.position.y,
-        //     oldPosition.z !== entity.scene.position.z)
-        let relativeToCameraUpdate = calculateViewClippingPointFromVector3(entity.scene.position)
-        adjustViewClipping(relativeToCameraUpdate.x, relativeToCameraUpdate.y)
+        //     oldPosition.z !== entity.scene.position.z
+        // )
+        while (oldPosition.x !== entity.scene.position.x || oldPosition.y !== entity.scene.position.y || oldPosition.z !== entity.scene.position.z) {
+            oldPosition = { x: entity.scene.position.x, y: entity.scene.position.y, z: entity.scene.position.z }
+            await sleep(1000 / 60) // Should probably follow the tick...
+            // console.log('entity.scene.position LOOP', oldPosition, entity.scene.position,
+            //     oldPosition.x !== entity.scene.position.x,
+            //     oldPosition.y !== entity.scene.position.y,
+            //     oldPosition.z !== entity.scene.position.z)
+            let relativeToCameraUpdate = calculateViewClippingPointFromVector3(entity.scene.position)
+            adjustViewClipping(relativeToCameraUpdate.x, relativeToCameraUpdate.y)
+        }
+        console.log('SCRLA finished')
+        return {}
+    } else {
+        console.log('SCRLA no entity, centering on screen')
+        const centre = {
+            x: window.currentField.metaData.assetDimensions.width / 2,
+            y: window.currentField.metaData.assetDimensions.height / 2
+        }
+        await tweenCameraPosition(getCurrentCameraPosition(), centre, tweenType, speed)
+        return {}
     }
-    console.log('SCRLA finished')
-    return {}
+
 }
 // Just for debug
-// setTimeout(async () => {
-//     await SCRLA({
-//         b: 0,
-//         s: 60,
-//         e: 20,
-//         t: 3
-//     })
-//     console.log('done')
-// }, 10000)
+setTimeout(async () => {
+    await SCRLA({
+        b: 0,
+        s: 60,
+        e: 202,
+        t: 3
+    })
+    console.log('done')
+}, 10000)
 
 // setTimeout(async () => {
 //     const entity = window.currentField.models.filter(m => m.userData.entityId === 20)[0]
