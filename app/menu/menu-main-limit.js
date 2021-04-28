5c92cd05b623c105562d0aa95eb51e1a410775f3
Dan Garfield <dangarfielduk@hotmail.com>
2021-04-28 09:40:59 +0100

    Limit menu - check implemented

--- app/menu/menu-main-limit.js
+++ app/menu/menu-main-limit.js
@@ -46,6 +46,9 @@ const loadLimitMenu = async partyMember => {
   })
   limitDescGroup = new THREE.Group()
   limitDescGroup.userData = { id: 6, z: 100 - 6 }
+  // limitDescGroup.position.x = 20
+  // limitDescGroup.position.y = -60
+  // limitDescGroup.userData.z = 100 - 6
   limitDesc.add(limitDescGroup)
   limitDesc.visible = true
   window.limitDescGroup = limitDescGroup
@@ -70,26 +73,23 @@ const loadLimitMenu = async partyMember => {
 
   drawAll(partyMember)
 
-  // await addTextToDialog(
-  //   limitActions,
-  //   `Limit menu - ${partyMember}`,
-  //   'home-loc',
-  //   LETTER_TYPES.MenuBaseFont,
-  //   LETTER_COLORS.White,
-  //   163,
-  //   225,
-  //   0.5
-  // )
   window.itemActions = limitActions
   await fadeOverlayOut(getHomeBlackOverlay())
-
-  setMenuState('limit')
-
-  movePointer(POINTERS.pointer1, 237, 17)
+  ACTION_POSITIONS.action = 0
+  loadActionSelection()
 }
 const LIMIT_DATA = {
-  member: 0
+  member: 0,
+  levelPositions: [
+    { x: 0, y: 0 },
+    { x: 146, y: 0 },
+    { x: 0, y: -68.5 },
+    { x: 146, y: -68.5 }
+  ],
+  level: 0,
+  limit: 0
 }
+window.LIMIT_DATA = LIMIT_DATA
 const drawAll = partyMember => {
   // Remove everything that was already drawn
   while (limitActionsGroup.children.length) {
@@ -211,8 +211,8 @@ const getLimitSkills = (char, potentialLimits, magicNameIndex, skip) => {
     skills.push([])
     for (let j = 0; j < potentialLimitLevel.length; j++) {
       const potentialLimit = potentialLimitLevel[j]
-      // if (char.limit.learnedLimitSkils.includes(potentialLimit)) {
-      if (true) {
+      if (char.limit.learnedLimitSkils.includes(potentialLimit)) {
+        // if (true) {
         // Just temp
         skills[i].push({
           name: window.data.kernel.magicNames[magicNameIndex + counter].replace(
@@ -237,26 +237,24 @@ const drawLimitList = (charName, char) => {
   LIMIT_DATA.skills = getAvailableLimitSkillsForCharacter(charName, char)
   console.log('limit drawLimitList', LIMIT_DATA.skills)
 
-  const skillGroupPositions = [
-    { x: 0, y: 0 },
-    { x: 146, y: 0 },
-    { x: 0, y: -68.5 },
-    { x: 146, y: -68.5 }
-  ]
   for (let i = 0; i < LIMIT_DATA.skills.length; i++) {
     const skillLevel = LIMIT_DATA.skills[i]
 
     const skillGroup = new THREE.Group()
-    skillGroup.position.x = skillGroupPositions[i].x
-    skillGroup.position.y = skillGroupPositions[i].y
+    skillGroup.position.x = LIMIT_DATA.levelPositions[i].x
+    skillGroup.position.y = LIMIT_DATA.levelPositions[i].y
     skillGroup.userData.z = limitListGroup.userData.z
     limitListGroup.add(skillGroup)
+    let color = LETTER_COLORS.Gray
+    if (skillLevel.length > 0) {
+      color = LETTER_COLORS.White
+    }
     addTextToDialog(
       skillGroup,
       'LEVEL ' + (i + 1),
       `limit-level-title-${i}`,
       LETTER_TYPES.MenuBaseFont,
-      LETTER_COLORS.White,
+      color,
       0,
       0,
       0.5
@@ -278,21 +276,168 @@ const drawLimitList = (charName, char) => {
   }
 }
 const exitMenu = async () => {
-  console.log('exitMenu')
   setMenuState('loading')
+  movePointer(POINTERS.pointer1, 0, 0)
   await fadeOverlayIn(getHomeBlackOverlay())
   limitActions.visible = false
   limitDesc.visible = false
   limitList.visible = false
   fadeInHomeMenu()
 }
+const clearDescription = () => {
+  while (limitDescGroup.children.length) {
+    limitDescGroup.remove(limitDescGroup.children[0])
+  }
+}
+const setDescription = description => {
+  clearDescription()
+  addTextToDialog(
+    limitDescGroup,
+    description,
+    'description',
+    LETTER_TYPES.MenuBaseFont,
+    LETTER_COLORS.White,
+    13, //10-24
+    72.5,
+    0.5
+  )
+}
+const ACTION_POSITIONS = {
+  x: [156, 196],
+  y: 50,
+  action: 0,
+  actions: ['Set', 'Check']
+}
+const loadActionSelection = () => {
+  setMenuState('limit-action-select')
+  movePointer(
+    POINTERS.pointer1,
+    ACTION_POSITIONS.x[ACTION_POSITIONS.action],
+    ACTION_POSITIONS.y
+  )
+  movePointer(POINTERS.pointer2, 0, 0, true, false)
+  clearDescription()
+}
+const limitActionConfirm = () => {
+  if (ACTION_POSITIONS.actions[ACTION_POSITIONS.action] === 'Check') {
+    loadCheckLevelSelection()
+  }
+}
+const limitActionNavigationToggle = () => {
+  ACTION_POSITIONS.action = ACTION_POSITIONS.action === 0 ? 1 : 0
+  loadActionSelection()
+}
+
+const loadCheckLevelSelection = () => {
+  setMenuState('limit-check-level')
+  movePointer(
+    POINTERS.pointer1,
+    ACTION_POSITIONS.x[ACTION_POSITIONS.action],
+    ACTION_POSITIONS.y,
+    false,
+    true
+  )
+  movePointer(
+    POINTERS.pointer2,
+    LIMIT_DATA.levelPositions[LIMIT_DATA.level].x + 10,
+    -LIMIT_DATA.levelPositions[LIMIT_DATA.level].y + 109
+  )
+  // TODO - set description
+  setDescription('Select LEVEL.')
+}
+
+const limitCheckLevelNavigation = horizontal => {
+  let newLevel = 0
+  if (LIMIT_DATA.level === 0 && horizontal) {
+    newLevel = 1
+  } else if (LIMIT_DATA.level === 1 && horizontal) {
+    newLevel = 0
+  } else if (LIMIT_DATA.level === 2 && horizontal) {
+    newLevel = 3
+  } else if (LIMIT_DATA.level === 3 && horizontal) {
+    newLevel = 2
+  } else if (LIMIT_DATA.level === 0 && !horizontal) {
+    newLevel = 2
+  } else if (LIMIT_DATA.level === 1 && !horizontal) {
+    newLevel = 3
+  } else if (LIMIT_DATA.level === 2 && !horizontal) {
+    newLevel = 0
+  } else if (LIMIT_DATA.level === 3 && !horizontal) {
+    newLevel = 1
+  }
+  if (LIMIT_DATA.skills.length > newLevel) {
+    LIMIT_DATA.level = newLevel
+    movePointer(
+      POINTERS.pointer2,
+      LIMIT_DATA.levelPositions[LIMIT_DATA.level].x + 10,
+      -LIMIT_DATA.levelPositions[LIMIT_DATA.level].y + 109
+    )
+  }
+}
+const limitCheckLevelConfirm = () => {
+  if (LIMIT_DATA.skills[LIMIT_DATA.level].length > 0) {
+    setMenuState('limit-check-limit')
+    LIMIT_DATA.limit = 0
+    limitCheckSelect()
+  }
+}
+const limitCheckLimitNavigationToggle = () => {
+  LIMIT_DATA.limit = 0
+  if (
+    LIMIT_DATA.skills[LIMIT_DATA.level].length > 1 &&
+    LIMIT_DATA.limit === 0
+  ) {
+    LIMIT_DATA.limit = 1
+  }
+  limitCheckSelect()
+}
+const limitCheckSelect = () => {
+  setDescription(
+    LIMIT_DATA.skills[LIMIT_DATA.level][LIMIT_DATA.limit].description
+  )
+  movePointer(
+    POINTERS.pointer2,
+    LIMIT_DATA.levelPositions[LIMIT_DATA.level].x + 24,
+    -LIMIT_DATA.levelPositions[LIMIT_DATA.level].y +
+      120.5 +
+      17 * LIMIT_DATA.limit
+  )
+}
 const keyPress = async (key, firstPress, state) => {
   console.log('press MAIN MENU LIMIT', key, firstPress, state)
-  if (state === 'limit') {
+  if (state === 'limit-action-select') {
     if (key === KEY.X) {
-      console.log('press MAIN MENU LIMIT EXIT')
-      movePointer(POINTERS.pointer1, 0, 0, true)
-      await exitMenu()
+      exitMenu()
+    } else if (key === KEY.O) {
+      limitActionConfirm()
+    } else if (key === KEY.LEFT) {
+      limitActionNavigationToggle()
+    } else if (key === KEY.RIGHT) {
+      limitActionNavigationToggle()
+    }
+  }
+  if (state === 'limit-check-level') {
+    if (key === KEY.X) {
+      loadActionSelection()
+    } else if (key === KEY.O) {
+      limitCheckLevelConfirm()
+    } else if (key === KEY.UP) {
+      limitCheckLevelNavigation(false)
+    } else if (key === KEY.DOWN) {
+      limitCheckLevelNavigation(false)
+    } else if (key === KEY.LEFT) {
+      limitCheckLevelNavigation(true)
+    } else if (key === KEY.RIGHT) {
+      limitCheckLevelNavigation(true)
+    }
+  }
+  if (state === 'limit-check-limit') {
+    if (key === KEY.X) {
+      loadCheckLevelSelection()
+    } else if (key === KEY.UP) {
+      limitCheckLimitNavigationToggle()
+    } else if (key === KEY.DOWN) {
+      limitCheckLimitNavigationToggle()
     }
   }
 }
