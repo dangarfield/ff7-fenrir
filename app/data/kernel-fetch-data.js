ffa493675b1863f1042e6f2e131a8905d5b18a42
Dan Garfield <dangarfielduk@hotmail.com>
2021-10-25 11:12:49 +0100

    Improve local asst caching and loading

--- app/data/kernel-fetch-data.js
+++ app/data/kernel-fetch-data.js
@@ -1,7 +1,7 @@
 import * as THREE from '../../assets/threejs-r118/three.module.js' //'https://cdnjs.cloudflare.com/ajax/libs/three.js/r118/three.module.min.js';
 import { setLoadingProgress } from '../loading/loading-module.js'
 const KUJATA_BASE = window.location.host.includes('localhost')
-  ? 'kujata-data'
+  ? 'http://localhost:3001/kujata-data'
   : 'https://kujata-data-dg.netlify.app'
 
 const windowTextures = {}
