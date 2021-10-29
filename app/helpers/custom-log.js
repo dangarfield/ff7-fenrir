aec0bc6e0a5ca41b8fe953f1caf4f01dcd3f2f1b
Dan Garfield <dangarfielduk@hotmail.com>
2021-10-29 22:17:47 +0100

    Log change

--- app/helpers/custom-log.js
+++ app/helpers/custom-log.js
@@ -10,7 +10,7 @@ window.console = (function (origConsole) {
       'press MAIN MENU HOME',
       'movePointer',
       'getBattleStatsForChar'
-      // 'status'
+      //'status'
 
     ],
     log: function () {
