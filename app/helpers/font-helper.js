2a6f2e663ea08bbdb46a01cec25b24e9c0e15acb
Dan Garfield <dangarfielduk@hotmail.com>
2022-12-08 11:45:49 +0000

    Clean font loader

--- app/helpers/font-helper.js
+++ app/helpers/font-helper.js
@@ -2,11 +2,9 @@ import { FontLoader } from '../../assets/threejs-r135-dg/examples/jsm/loaders/Fo
 
 const loadFont = async () => {
   return new Promise((resolve, reject) => {
-    console.log('fontloader')
     new FontLoader().load(
       'assets/threejs-r135-dg/examples/fonts/helvetiker_regular.typeface.json',
       font => {
-        console.log('fontloader complete', font)
         resolve(font)
       }
     )
