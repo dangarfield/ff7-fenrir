be20306f3a2bdb41557307f6eda2c0c58efb538d
Dan Garfield <dangarfielduk@hotmail.com>
2022-12-21 09:51:35 +0000

    Battle controls updated for pause

--- app/battle/battle-controls.js
+++ app/battle/battle-controls.js
@@ -1,7 +1,7 @@
 import { getKeyPressEmitter } from '../interaction/inputs.js'
 import { setLastBattleResult } from '../field/field-battle.js'
 import { resolveBattlePromise } from './battle-module.js'
-import { togglePauseBattle } from './battle-scene.js'
+import { togglePauseBattle, BATTLE_PAUSED } from './battle-scene.js'
 
 const areBattleControlsActive = () => {
   return window.anim.activeScene === 'battle'
@@ -9,13 +9,13 @@ const areBattleControlsActive = () => {
 
 const initBattleKeypressActions = () => {
   getKeyPressEmitter().on('o', firstPress => {
-    if (areBattleControlsActive() && firstPress) {
+    if (areBattleControlsActive() && firstPress && !BATTLE_PAUSED) {
       console.log('press o')
     }
   })
 
   getKeyPressEmitter().on('x', firstPress => {
-    if (areBattleControlsActive() && firstPress) {
+    if (areBattleControlsActive() && firstPress && !BATTLE_PAUSED) {
       console.log('press x')
       // Temp
       setLastBattleResult(true, false)
@@ -25,7 +25,6 @@ const initBattleKeypressActions = () => {
   getKeyPressEmitter().on('start', firstPress => {
     if (areBattleControlsActive() && firstPress) {
       console.log('press start')
-      // Temp
       togglePauseBattle()
     }
   })
